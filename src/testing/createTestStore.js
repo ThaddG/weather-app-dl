65873dc0cea9a5b65518eed60e80681b5eb8243f
ThaddG <thaddious.gorges@gmail.com>
2021-09-05 21:30:20 -0400

    remvoed unused reducer and route component

--- src/testing/createTestStore.js
+++ src/testing/createTestStore.js
@@ -1,13 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { authReducer } from '../redux/reducers/authReducer';
 import { locationReducer } from '../redux/reducers/locationReducer';
 import { temperatureReducer } from '../redux/reducers/temperatureReducer';
 
 export default function createTestStore() {
   const store = createStore(
     combineReducers({
-      auth: authReducer,
       location: locationReducer,
       temperature: temperatureReducer,
     }),
